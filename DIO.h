5f6d2192712e8d8c387c1394c9fa7745f2f6997b
samirMossad1 <you@example.com>
2019-03-07 16:39:16 +0200

    added the Dio_level enum , modified the return and the pass parameter of some functions

--- DIO.h
+++ DIO.h
@@ -1,11 +1,24 @@
 #ifndef DIO_H
 #define DIO_H
 
-char DIO_ReadPort(uint8 port_index , uint8 pins_mask);
+
+typedef enum
+{
+
+STD_LOW,STD_HIGH
+
+
+}DIO_LevelType;
+
+
+
+
+
+uint8 DIO_ReadPort(uint8 port_index , uint8 pins_mask);
 /*Return the value of the pins selected by
 pins_masks in the port selected by port_index*/
 
-void DIO_WritePort(uint8 port_index , uint8 pins_mask, char pins_level);
+void DIO_WritePort(uint8 port_index , uint8 pins_mask, DIO_LevelType pin_level);
 /*change the value of the pins selected by pins_masks
 in the port selected by port_index to input pins_level*/
 

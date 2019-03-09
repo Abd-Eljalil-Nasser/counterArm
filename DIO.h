8e0b012e77d25b0ba51707ad122b6aea8676bfbc
zyad mohamed <zezo19975@yahoo.com>
2019-03-09 12:45:43 +0200

    Update DIO.h

--- DIO.h
+++ DIO.h
@@ -20,15 +20,15 @@ STD_LOW,STD_HIGH
 
 
 
-uint8 DIO_ReadPort(uint8 port_index , uint8 pins_mask);
+uint8 DIO_ReadPort(Port port_index , uint8 pins_mask);
 /*Return the value of the pins selected by
 pins_masks in the port selected by port_index*/
 
-void DIO_WritePort(uint8 port_index , uint8 pins_mask, DIO_LevelType pin_level);
+void DIO_WritePort(Port port_index , uint8 pins_mask, DIO_LevelType pin_level);
 /*change the value of the pins selected by pins_masks
 in the port selected by port_index to input pins_level*/
 
-void DIO_FlipPort (uint8 port_index, uint8 pins_mask);
+void DIO_FlipPort (Port port_index, uint8 pins_mask);
 /*Toggle the values of the pins selected by pin_masks
 in the port selected by port_index*/
 

d282d048a4fac4baa03d855887fded3c7ef4a02b
Abd-El Jalil Nasser <36386271+Abd-Eljalil-Nasser@users.noreply.github.com>
2019-03-09 15:14:03 +0200

    Update DIO.c
    read_port added

--- DIO.c
+++ DIO.c
@@ -96,5 +96,38 @@ void DIO_WritePort(uint8 port_index , uint8 pins_mask, DIO_LevelType pin_level){
    
         
     }
+	//DIO_ReadPort
+/*Return the value of the pins selected by
+pins_masks in the port selected by port_index*/
+uint8 DIO_ReadPort (uint8 port_index, uint8 pins_mask) {
+    switch (port_index) {
+	
+        // Port A
+        case 0 : return GPIO_PORTA_DATA_R & pins_mask;
+    
+
+        // Port B
+        case 1 : return  GPIO_PORTB_DATA_R & pins_mask;
+       
+
+        // Port C
+        case 2 : return GPIO_PORTC_DATA_R & pins_mask;
+				
+
+        // Port D
+        case 3 : return GPIO_PORTD_DATA_R & pins_mask;      
+       
+
+        // Port E
+        case 4 : return GPIO_PORTE_DATA_R & pins_mask; 
+        
+        
+        // Port F 
+        case 5 : return GPIO_PORTF_DATA_R & pins_mask;  
+       
+		
+		}
+		
+		}
 }
 	
